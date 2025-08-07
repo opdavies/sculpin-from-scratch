3e334029134e1ea45417589f49126bf00d4fd51c
Oliver Davies <oliver@oliverdavies.uk>
2025-08-07 01:53:21 +0100

    Add justfile

--- flake-modules/book.nix
+++ flake-modules/book.nix
@@ -6,44 +6,7 @@
         packages = with pkgs; [
           asciidoctor-with-extensions
           coreutils
-        ];
-
-        commands = [
-          {
-            name = "create-book-all";
-            command = ''
-              create-book-html
-              create-book-pdf
-              create-book-epub
-            '';
-          }
-
-          {
-            name = "create-book-epub";
-            command = ''
-              asciidoctor-epub3 \
-                -a date="$(date +'%Y-%m-%d %H:%M')" index.adoc \
-                  --out-file build/sculpin-from-scratch.epub
-            '';
-          }
-
-          {
-            name = "create-book-html";
-            command = ''
-              asciidoctor \
-                -a date="$(date +'%Y-%m-%d %H:%M')" index.adoc \
-                  --out-file build/sculpin-from-scratch.html
-            '';
-          }
-
-          {
-            name = "create-book-pdf";
-            command = ''
-              asciidoctor-pdf \
-                -a date="$(date +'%Y-%m-%d %H:%M')" index.adoc \
-                  --out-file build/sculpin-from-scratch.pdf
-            '';
-          }
+          just
         ];
       };
     };
